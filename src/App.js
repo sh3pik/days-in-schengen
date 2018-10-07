0c24e1c2ef448e61ebfa1c345dabd80d550648ab
Tatiana Shepeleva <tatiana@toptal.com>
2018-10-07 10:14:33 +0200

    Add form with date fields

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import DatesForm from './datesForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Schengen Calculator</h1>
         </header>
         <p className="App-intro">
           The app will help you to calculate how many days you can stay in
           Schegen zone and not to break any rules.
         </p>
+        <DatesForm />
       </div>
     )
   }
---
+++ src/datesForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import parse from 'date-fns/parse'
+import format from 'date-fns/format'
+import { usedDays } from './calculateDays'
+
+export default class DatesForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { dates: [], spendDays: 0 }
+  }
+
+  updateDate(index, key, newValue) {
+    const newDates = Object.assign([], this.state.dates)
+    newDates[index] = newDates[index] || {}
+    newDates[index][key] = newValue
+    this.setState({ dates: newDates })
+  }
+
+  showDays(e) {
+    e.preventDefault()
+    const spendDays = usedDays(
+      this.state.dates.map(({ start, end }) => ({
+        start: parse(start),
+        end: parse(end)
+      }))
+    )
+    this.setState({ spendDays })
+  }
+
+  render() {
+    let result = ''
+    const spendDays = this.state.spendDays
+    if (spendDays !== 0) {
+      const text = spendDays == 1 ? ' day spend' : ' days spend'
+      result = spendDays + text
+    }
+    return (
+      <form onSubmit={this.showDays.bind(this)}>
+        <p>Write days in mm-dd-yyyy format</p>
+        <label>Day In:</label>
+        {this.state.dates
+          .concat({ start: null, end: null })
+          .map(({ start, end }, index) => (
+            <div key={index.toString()}>
+              <input
+                onChange={e => this.updateDate(index, 'start', e.target.value)}
+              />
+              <input
+                onChange={e => this.updateDate(index, 'end', e.target.value)}
+              />
+            </div>
+          ))}
+
+        <button>Press me</button>
+        <span>{result}</span>
+      </form>
+    )
+  }
+}
